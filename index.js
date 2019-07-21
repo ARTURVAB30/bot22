53f37bdd522c11f502dcec74c378b2b4f657b2eb
ARTURVAB30 <51975405+ARTURVAB30@users.noreply.github.com>
2019-07-21 17:52:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const fs = require('fs');
+
+
+
+client.on("ready", () => {
+  client.user.setPresence({ game: { name: `CLÃ FENIX`, url: "https://www.twitch.tv/CLÃFENIX", type: 1 } });
+  console.log("Online!")
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+
+});
+
+client.login(config.token);
+console.log("Logado!");
+
+client.on('message', message => {
+  if (message.content == '<@539980207533064219>') {
+    message.channel.send(`Minha prefix é \`${config.prefix}\`, use **/ajuda**.`)
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  let embed = new Discord.RichEmbed()
+    .setTitle("Bem-Vindo")
+    .setColor("RANDON")
+    .setThumbnail(member.user.avatarURL)
+    .setDescription(`${member}! Você acabou de entrar no servidor!`)
+    .setFooter(`ID do membro: ${member.id}`, member.user.avatarURL);
+
+  let canalEntrada = client.channels.get("584806117604065290")
+    .send(embed);
+  
+});
+
+client.on('guildMemberRemove', member => {
+  let embed = new Discord.RichEmbed()
+    .setTitle("Saiu!")
+    .setColor("RANDON")
+    .setThumbnail(member.user.avatarURL)
+    .setDescription(`${member}! Acabou de sair do servidor!`)
+    .setFooter(`ID do membro: ${member.id}`, member.user.avatarURL);
+
+  let canalEntrada = client.channels.get("584806117604065290")
+    .send(embed);
+
+});
+
+client.login(config.token);
